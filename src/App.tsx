9c18ab812dbd8ded7edf12954cd74b338b48550c
hicarenet <hicare@hicarenetui-MacBook-Pro.local>
2024-04-30 11:49:12 +0900

    feat: useForm hook을 사용하여 patientSchema를 resolver로 하는  form 생성
    기본UI 추가
    Input, SelectBox

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,27 @@
+import { PatientSchemaType, patientSchema } from "./Shema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Input, MenuItem, Select } from "@mui/material";
+import { useForm } from "react-hook-form";
+
 function App() {
-  return <div>여기에 작업해주세요</div>;
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<PatientSchemaType>({
+    resolver: zodResolver(patientSchema),
+  });
+
+  return (
+    <div>
+      <Input />
+      <Select>
+        <MenuItem>box1</MenuItem>
+        <MenuItem>box2</MenuItem>
+        <MenuItem>box3</MenuItem>
+      </Select>
+    </div>
+  );
 }
 
 export default App;
