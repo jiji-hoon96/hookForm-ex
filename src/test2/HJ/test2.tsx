1c4a9cee989396ab96999625263d91cfb1afb9de
hyun <ysk30430@naver.com>
2024-05-02 16:19:01 +0900

    style : UI 초안

--- src/test2/HJ/test2.tsx
+++ src/test2/HJ/test2.tsx
@@ -1,5 +1,61 @@
 function Test2HJ() {
-  return <div>김현지씨 작업 파일입니다</div>;
+  return (
+    <>
+      <section>
+        <p>Insurance</p>
+        <div>
+          <input type="text" />
+          <button></button>
+          <input type="radio" />
+          <input type="date" />
+          <select></select>
+          <input type="text"></input>
+          <input type="text"></input>
+          <input type="text"></input>
+        </div>
+      </section>
+      <section>
+        <p>Patient profile</p>
+        <div>
+          <select></select>
+          <input type="text"></input>
+          <input type="text"></input>
+          <input type="text"></input>
+          <select></select>
+          <select></select>
+          <input type="date"></input>
+          <input type="number"></input>
+          <input type="text"></input>
+          <button></button>
+          <select></select>
+          <select></select>
+          <textarea></textarea>
+          <select></select>
+        </div>
+      </section>
+      <section>
+        <p>Contact</p>
+        <div>
+          <select></select>
+          <input type="number"></input>
+          <button></button>
+          <input type="email"></input>
+          <button></button>
+          <input type="text"></input>
+          <input type="text"></input>
+          <input type="text"></input>
+          <select></select>
+          <input type="number"></input>
+          <input type="text"></input>
+          <input type="text"></input>
+          <input type="text"></input>
+          <input type="text"></input>
+          <input type="text"></input>
+        </div>
+        <button></button>
+      </section>
+    </>
+  );
 }
 
 export default Test2HJ;
