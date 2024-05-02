2d49bef8589cf4bbdb0bbbb15a69b991f5e6316b
JIHOON LEE <jihoon7705@gmail.com>
2024-05-02 14:30:59 +0900

    fix : 데이터 수정

--- src/App.tsx
+++ src/App.tsx
@@ -8,13 +8,8 @@ import {
   RadioGroup,
   Select,
 } from "@mui/material";
-import { DatePicker } from "@mui/x-date-pickers/DatePicker";
-import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
-import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
-import dayjs from "dayjs";
-import { Controller, useForm } from "react-hook-form";
+import { useForm } from "react-hook-form";
 
-const dateFormat = dayjs().format("DD/MM/YYYY");
 const selectValue = ["box1", "box2", "box3"];
 const radioValue = ["option1", "option2", "option3"];
 function App() {
@@ -22,7 +17,6 @@ function App() {
     register,
     watch,
     handleSubmit,
-    control,
     formState: { errors },
   } = useForm<PatientSchemaType>({
     resolver: zodResolver(patientSchema),
@@ -64,28 +58,7 @@ function App() {
       </section>
 
       <section>
-        <Input type='date' {...register("date")} />
-        <Controller
-          control={control}
-          name='date'
-          render={({ field }) => (
-            <LocalizationProvider dateAdapter={AdapterDayjs} dateFormats={MM/>}>
-              <DatePicker
-                {...field}
-                format='MM/DD/YYYY'
-                slotProps={{
-                  textField: {
-                    variant: "standard",
-                  },
-                }}
-                onChange={(date) => {
-                  AdapterDayjs.format(date, "MM/DD/YYYY");
-                }}
-              />
-            </LocalizationProvider>
-          )}
-        />
-        {errors.date && <p style={{ color: "red" }}>{errors.date.message}</p>}
+        <Input type="date" {...register("date")} />
       </section>
 
       <section>
@@ -95,9 +68,9 @@ function App() {
               value === "" ? undefined : value;
             },
           })}
-          defaultValue='defaultValue'
+          defaultValue="defaultValue"
         >
-          <MenuItem disabled value='defaultValue'>
+          <MenuItem disabled value="defaultValue">
             선택해주세요
           </MenuItem>
           {selectValue.map((value, index) => (
@@ -122,8 +95,8 @@ function App() {
       </section>
 
       <section>
-        <button type='reset'>리셋</button>
-        <button type='submit'>제출</button>
+        <button type="reset">리셋</button>
+        <button type="submit">제출</button>
       </section>
     </form>
   );
